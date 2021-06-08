194020e25b1c123951fe3129ea656bb9ebfa688f
Kacper <kacpergierycz@gmail.com>
2021-06-08 14:06:18 +0200

    form with paramether for post method and print on html

--- SimpleServletProject/src/firstservlet/XmlServlet.java
+++ SimpleServletProject/src/firstservlet/XmlServlet.java
@@ -1,6 +1,7 @@
 package firstservlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,22 @@ public class XmlServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("XML servlet called");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName= request.getParameter("userName");
+		out.println("Hello! from Get method User! "+userName);
+	
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("XML servlet called");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName= request.getParameter("userName");
+		out.println("Hello! from Post method User! "+userName);
+	
+		
 	}
 
 }
